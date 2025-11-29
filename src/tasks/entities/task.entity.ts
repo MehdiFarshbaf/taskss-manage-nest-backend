55cabdc22269f31678afeb50933eac1360f65349
Mehdi Farshbaf <mehdifarshbaf92@gmail.com>
2025-11-29 14:30:05 +0330

    creat entities

---
+++ src/tasks/entities/task.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import TaskStatusEnum from '../enums/taskStatus.Enum';
+
+@Entity({ name: 'tasks' })
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+  @Column()
+  description: string;
+
+  @Column({ type: 'enum', enum: TaskStatusEnum, default: TaskStatusEnum.Set })
+  status: TaskStatusEnum;
+}
